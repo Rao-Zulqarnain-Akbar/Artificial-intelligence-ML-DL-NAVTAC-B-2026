7f1d9fc24f0991c61f9630e8a0cfc4ec4447409b
Rao-Zulqarnain-Akbar <158838624+Rao-Zulqarnain-Akbar@users.noreply.github.com>
2026-03-04 00:21:17 +0500

    Add files via upload

---
+++ week2 work/sqr_function.py
@@ -0,0 +1,20 @@
+def calculate_square (x):
+    y = x*x
+    print("Square of number is: ",y)
+num =int(input("Enter the number: " ))
+calculate_square(num)
+
+def calculate_cube (x):
+    y = x**3
+    print("cube of number is: ",y)
+num =int(input("Enter the number: " ))
+calculate_cube(num)
+
+def calc_sum (x,y):
+    sum = x+y
+    print("The sum is: ", sum)
+num1 = int(input("Enter the number1: "))
+num2 = int(input("Enter the number2: "))
+calc_sum(num1,num2)
+
+
